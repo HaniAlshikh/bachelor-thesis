20d811cf09f6810d12419b3fe0101bebd40abd16
Hani Alshikh <hani.alshikh@haw-hamburg.de>
2023-02-16 21:49:53 +0100

    Finalise installation and configuration formulation (#22)
    Signed-off-by: HaniAlshikh <hani.alshikh@haw-hamburg.de>

---
+++ deploy/README.md
@@ -0,0 +1,113 @@
+# deploy
+
+Deploy Monoskope and MonoGUI to a local cluster.
+
+The [deploy](deploy.mk) makefile will:
+
+1. take care of the preparation work
+2. create local Kubernetes cluster using `kind`
+3. add Monoskope and MonoGUI dependencies to `helm`
+4. setup and deploy all needed resources
+
+All while making sure to stay idempotent and localized.
+
+## Prerequisites
+
+### Required
+
+A supported system from the following:
+
+- MacOS
+- Linux (tested on ubuntu)
+- Windows (WSL)
+
+Standard userspace utilities like `bash`, `curl`, `sh`, etc.... You can always change to alternatives by setting the corresponding environment variable. For example replacing `curl` with `wget` will be as follows:
+
+```shell
+CURL=wget make deploy
+```
+
+And the following tools
+
+* [docker](https://docs.docker.com/get-docker/) to create isolated environments
+* [make](https://www.gnu.org/software/make/#download) to run deployment scripts
+
+### Automated
+
+the following will be downloaded and configured locally. If any should fail please follow the same instruction as the curl example above, when done downloading and installing manually.
+
+* [kubectl](https://kubernetes.io/docs/tasks/tools/#kubectl) to manage Kubernetes (k8s) resources
+* [kind](https://kind.sigs.k8s.io/docs/user/quick-start/#installation) to create a local k9s cluster
+* [helm](https://helm.sh/docs/intro/install/) to generate resource definitions
+* [step-cli](https://smallstep.com/docs/step-cli/installation) to generate m8 PKI's trust-anchor
+
+## Test Run
+
+> **_IMPORTENTE:_**  If you decided to make changes on the [setup files](setup) please make sure to do a wide search and replace all occurrences if applicable.
+
+1. Deploy all resources to a local cluster:
+
+    ```shell
+    make deploy
+    # you can also run the following to monitor the state of the resources
+    make kind-watch
+    ```
+
+2. Trust m8 domain certificate: `tmp/domain-ca.crt`, otherwise the browser will block communication with m8 API
+
+    ```shell
+    # MacOS
+    sudo security add-trusted-cert -d -r trustRoot -k "/Library/Keychains/System.keychain" tmp/domain-ca.crt
+    ```
+
+3. Create port-forwards to route local request to backing services in the cluster: (Make sure `8443`, `5556` and `3000` are not in use)
+
+    ```shell
+    make port-forward
+    ```
+
+4. Add the following to your hosts file:
+
+    ```shell
+    127.0.0.1 api.monoskope.dev
+    127.0.0.1 dex
+    ```
+
+5. Populate the EventStore with some mock data for a better user experience
+
+    ```shell
+    make mock-data
+    ```
+
+## Makefile
+
+```shell
+Usage:
+  make <target>
+
+General
+  help                      Display this help.
+
+Manage
+  kind-watch                watch monoskope beain deployed
+  port-forward              create a port-forward to the m8Api, dex and monogui
+  mock-data                 restore mocked data into monoskope backing database
+
+Deploy
+  deploy                    deploy monoskope and monogui
+  kind-create-cluster       create kind cluster
+  deploy-m8-trust-anchor    create trust-anchor in kind cluster
+  deploy-cert-manager       deploy dex
+  deploy-emissary-ingress   deploy emissary-ingress
+  deploy-dex                deploy dex
+  deploy-monoskope          deploy monoskope
+  deploy-monogui            deploy monoskope
+  deploy-cleanup            uninstall everything again
+
+Build Dependencies          
+  kubectl                   Download kubectl locally if necessary.
+  kind                      Download kind locally if necessary.
+  step                      Download step locally if necessary.
+  helm                      Download helm locally if necessary.
+  helm-repo                 add necessary chart repos
+```
---
+++ deploy/deploy.mk
@@ -0,0 +1,170 @@
+# kind
+K8S_VERSION=1.24.7
+KIND_KUBECONFIG = $(LOCALTMP)/kind-kubeconfig
+KIND_M8_CLUSTER_NAME ?= m8-dev-cluster
+
+# values
+CERT_MANAGER_HELM_VALUES_FILE		?= setup/cert-manager.yaml
+CERT_MANAGER_ISSUER_RESOURCE_FILE		?= setup/cert-manager-issuer.yaml
+EMISSARY_INGRESS_HELM_VALUES_FILE		?= setup/emissary-ingress.yaml
+DEX_HELM_VALUES_FILE		?= setup/dex.yaml
+MONOSKOPE_HELM_VALUES_FILE		?= setup/m8.yaml
+EMISSARY_INGRESS_LISTENER_RESOURCE_FILE		?= setup/emissary-ingress-listener.yaml
+MONOGUI_HELM_VALUES_FILE		?= setup/monogui.yaml
+
+MOCK_DATA_RESOURCE_FILE		?= setup/m8-eventstore-mock-data.yaml
+
+# versions
+CERT_MANAGER_VERSION ?= v1.11.0
+EMISSARY_VERSION ?= 3.4.0
+EMISSARY_INGRESS_CHART_VERSION ?= 8.4.0
+DEX_VERSION ?= 0.8.2
+MONOSKOPE_VERSION ?= v0.5.2
+MONOGUI_VERSION ?= v0.1.0-rc02
+
+
+KIND_KUBECTL ?= $(KUBECTL) --kubeconfig ${KIND_KUBECONFIG}
+KIND_HELM ?= $(HELM) --kubeconfig ${KIND_KUBECONFIG}
+
+##@ Manage
+
+.PHONY: kind-watch
+kind-watch: kubectl ## watch monoskope beain deployed
+	@watch "$(KIND_KUBECTL) get no -owide; echo; $(KIND_KUBECTL) get po -A"
+
+.PHONY: port-forward
+port-forward: kubectl ## create a port-forward to the m8Api, dex and monogui
+	@KUBECTL=$(KUBECTL) KUBECONFIG=$(KIND_KUBECONFIG) sh ./port-forward.sh
+
+.PHONY: mock-data
+mock-data: kubectl ## restore mocked data into monoskope backing database
+	@$(KIND_KUBECTL) -n monoskope delete -f $(MOCK_DATA_RESOURCE_FILE) 2>/dev/null || true
+	@$(KIND_KUBECTL) -n monoskope apply -f $(MOCK_DATA_RESOURCE_FILE)
+
+##@ Deploy
+
+.PHONY: deploy
+deploy: deploy-cleanup kind-create-cluster helm-repo deploy-m8-trust-anchor deploy-cert-manager deploy-emissary-ingress deploy-dex deploy-monoskope deploy-monogui ## deploy monoskope and monogui
+
+
+.PHONY: kind-create-cluster
+kind-create-cluster: kind ## create kind cluster
+	@$(KIND) create cluster --name ${KIND_M8_CLUSTER_NAME} --image kindest/node:v${K8S_VERSION} --kubeconfig ${KIND_KUBECONFIG}
+	@$(KIND) get kubeconfig --name ${KIND_M8_CLUSTER_NAME} > ${KIND_KUBECONFIG}
+
+.PHONY: deploy-m8-trust-anchor
+deploy-m8-trust-anchor: kubectl step ## create trust-anchor in kind cluster
+	@$(ECHO) "Generating trust-anchor for m8 PKI..."
+	@$(STEP) certificate create root.monoskope.cluster.local $(LOCALTMP)/ca.crt $(LOCALTMP)/ca.key --profile root-ca --no-password --insecure --not-after=87600h
+	@$(ECHO) "Creating secret containing trust-anchor in kind cluster..."
+	@$(KIND_KUBECTL) create namespace monoskope --dry-run=client -o yaml | $(KIND_KUBECTL) apply -f -
+	@$(KIND_KUBECTL) -n monoskope create secret tls m8-trust-anchor --cert=$(LOCALTMP)/ca.crt --key=$(LOCALTMP)/ca.key --dry-run=client -o yaml | $(KIND_KUBECTL) apply -f -
+
+.PHONY: deploy-cert-manager
+deploy-cert-manager: helm kubectl helm-repo ## deploy dex
+	@$(KIND_HELM) upgrade -i cert-manager -n cert-manager --create-namespace jetstack/cert-manager --version $(CERT_MANAGER_VERSION) --values $(CERT_MANAGER_HELM_VALUES_FILE)
+	@$(KIND_KUBECTL) apply -f $(CERT_MANAGER_ISSUER_RESOURCE_FILE)
+
+.PHONY: deploy-emissary-ingress
+deploy-emissary-ingress: kubectl helm helm-repo ## deploy emissary-ingress
+	@$(KIND_KUBECTL) apply -f https://app.getambassador.io/yaml/emissary/$(EMISSARY_VERSION)/emissary-crds.yaml
+	@$(KIND_KUBECTL) -n emissary-system patch deploy emissary-apiext -p '{"spec":{"replicas": 1}}'
+	@$(KIND_KUBECTL) wait --timeout=90s --for=condition=available deployment emissary-apiext -n emissary-system
+	@$(KIND_HELM) upgrade -i ei -n emissary --create-namespace datawire/emissary-ingress --version $(EMISSARY_INGRESS_CHART_VERSION) --values $(EMISSARY_INGRESS_HELM_VALUES_FILE) \
+		--set=image.tag=$(EMISSARY_VERSION)
+
+.PHONY: deploy-dex
+deploy-dex: kubectl helm helm-repo ## deploy dex
+	@$(KIND_HELM) upgrade -i dex -n monoskope --create-namespace dex/dex --version $(DEX_VERSION) --values $(DEX_HELM_VALUES_FILE)
+	@$(KIND_KUBECTL) -n monoskope create secret generic m8-gateway-oidc --from-literal=oidc-clientid=gateway --from-literal=oidc-clientsecret=verysecretclientsecret --from-literal=oidc-nonce=somerandomstring --dry-run=client -o yaml | $(KIND_KUBECTL) apply -f -
+
+.PHONY: deploy-monoskope
+deploy-monoskope: helm kubectl helm-repo ## deploy monoskope
+	@$(KIND_HELM) upgrade -i m8 -n monoskope --create-namespace finleap-connect/monoskope --version $(MONOSKOPE_VERSION) --values $(MONOSKOPE_HELM_VALUES_FILE)
+	@$(KIND_KUBECTL) apply -f $(EMISSARY_INGRESS_LISTENER_RESOURCE_FILE)
+	@$(KIND_KUBECTL) wait --timeout=90s --for=condition=ready certificate m8-monoskope-tls-cert -n monoskope
+	@$(KIND_KUBECTL) -n monoskope get secret m8-monoskope-tls-cert -o jsonpath='{.data.ca\.crt}' | base64 -D > $(LOCALTMP)/domain-ca.crt
+	@$(ECHO) "server: api.monoskope.dev:8443" > $(LOCALTMP)/monoctl-devconfig
+
+.PHONY: deploy-monogui
+deploy-monogui: helm kubectl helm-repo ## deploy monoskope
+	@$(KIND_HELM) upgrade -i monogui -n monoskope --create-namespace H-S/monogui --version $(MONOGUI_VERSION) --values $(MONOGUI_HELM_VALUES_FILE)
+
+.PHONY: deploy-cleanup
+deploy-cleanup: kind ## uninstall everything again
+	@$(ECHO) "cleaning up..."
+	@$(KIND) delete cluster --name ${KIND_M8_CLUSTER_NAME}
+	@$(RM) -R $(LOCALTMP) || true
+
+
+##@ Build Dependencies
+ARCH := $(shell sh -c 'uname -m 2>/dev/null || echo not')
+OS := $(shell sh -c 'uname -s 2>/dev/null | tr A-Z a-z || echo not')
+
+# kubectl
+KUBECTL ?= $(LOCALBIN)/kubectl
+KUBECTL_VERSION ?= $(K8S_VERSION)
+KUBECTL_OS = $(OS)
+KUBECTL_ARCH = $(ARCH)
+ifeq ($(OS),darwin)
+	ifeq ($(ARCH),x86_64)
+	KUBECTL_ARCH = amd64
+	endif
+endif
+
+kubectl: $(KUBECTL) ## Download kubectl locally if necessary.
+$(KUBECTL): $(LOCALBIN)
+	@$(CURL) -L -o $(KUBECTL) "https://dl.k8s.io/release/v$(KUBECTL_VERSION)/bin/$(KUBECTL_OS)/$(KUBECTL_ARCH)/kubectl"
+	@chmod +x $(KUBECTL)
+
+# kind
+KIND ?= $(LOCALBIN)/kind
+KIND_VERSION ?= 0.17.0
+KIND_OS = $(KUBECTL_OS)
+KIND_ARCH = $(KUBECTL_ARCH)
+
+kind: $(KIND) ## Download kind locally if necessary.
+$(KIND): $(LOCALBIN)
+	@$(CURL) -L -o $(KIND) "https://kind.sigs.k8s.io/dl/v$(KIND_VERSION)/kind-$(KIND_OS)-$(KIND_ARCH)"
+	@chmod +x $(KIND)
+
+# step
+STEP ?= $(LOCALBIN)/step
+STEP_VERSION ?= 0.23.2
+STEP_OS = $(KUBECTL_OS)
+STEP_ARCH = $(KUBECTL_ARCH)
+
+step: $(STEP) ## Download step locally if necessary.
+$(STEP): $(LOCALBIN)
+	@$(CURL) -L -o step.tar.gz "https://dl.step.sm/gh-release/cli/gh-release-header/v$(STEP_VERSION)/step_$(STEP_OS)_$(STEP_VERSION)_$(STEP_ARCH).tar.gz"
+	@$(TAR) -zxvf step.tar.gz -C $(LOCALBIN) --strip-components=2 "step*/bin/step"
+	@$(RM) step.tar.gz
+	@chmod +x $(STEP)
+
+# helm
+HELM ?= $(LOCALBIN)/helm
+HELM_VERSION ?= 3.11.1
+HELM_HOME ?= $(LOCALBIN)/helm-home
+$(HELM_HOME): $(LOCALBIN)
+	mkdir -p $(HELM_HOME)
+export HELM_REGISTRY_CONFIG = $(HELM_HOME)/config.json
+export HELM_REPOSITORY_CACHE = $(HELM_HOME)
+export HELM_REPOSITORY_CONFIG = $(HELM_HOME)/repositories.yaml
+export HELM_PLUGINS = $(HELM_HOME)
+export HELM_DATA_HOME = $(HELM_HOME)
+export HELM_CONFIG_HOME = $(HELM_HOME)
+export HELM_CACHE_HOME = $(HELM_HOME)
+
+helm: $(HELM) ## Download helm locally if necessary.
+$(HELM): $(LOCALBIN) $(HELM_HOME)
+	@$(CURL) https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | HELM_INSTALL_DIR=$(LOCALBIN) DESIRED_VERSION=v$(HELM_VERSION) USE_SUDO="false" $(BASH)
+	touch $(HELM_REPOSITORY_CONFIG)
+
+helm-repo: helm $(HELM_REGISTRY_CONFIG) ## add necessary chart repos
+$(HELM_REGISTRY_CONFIG): $(HELM_HOME)
+	@$(HELM) repo add jetstack https://charts.jetstack.io
+	@$(HELM) repo add datawire https://getambassador.io
+	@$(HELM) repo add dex https://charts.dexidp.io
+	@$(HELM) repo add finleap-connect https://finleap-connect.github.io/charts/
+	@$(HELM) repo add H-S https://charts.alshikh.de
+	@$(HELM) repo update
