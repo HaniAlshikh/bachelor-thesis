c6694e5c0313e8efbd87a74179c532ddff0803aa
HaniAlshikh <hani.alshikh@haw-hamburg.de>
2023-02-28 04:44:43 +0100

    finilise deploy
    Signed-off-by: HaniAlshikh <hani.alshikh@haw-hamburg.de>

--- deploy/port-forward.sh
+++ deploy/port-forward.sh
@@ -6,7 +6,7 @@ echo "Forwarding m8 API at 8443..."
 $KUBECTL --kubeconfig $KUBECONFIG -n emissary port-forward svc/ei-emissary-ingress 8443:443 &
 echo "Forwarding dex API at 5556..."
 $KUBECTL --kubeconfig $KUBECONFIG -n monoskope port-forward svc/dex 5556 &
-# echo "Forwarding monogui at 3000..."
-# $KUBECTL --kubeconfig $KUBECONFIG -n monoskope port-forward svc/monogui 3000:80 &
+echo "Forwarding monogui at 3000..."
+$KUBECTL --kubeconfig $KUBECONFIG -n monoskope port-forward svc/monogui 3000:80 &
 
 wait
